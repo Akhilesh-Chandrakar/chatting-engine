5edcadecd8f782218c45d61771154b1ac6dd87b5
Akhilesh-Chandrakar <akhil79022@gmail.com>
2022-11-19 15:15:51 +0530

    added controllers and routes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app=express();
 app.use('/', require('./routes'));
 app.listen(port, function(err){
     if(err){
-        console.log("error at connceting to server", err);
+        console.log("error in connceting to server", err);
     }
     console.log('server is connected at port:', port);
 })
